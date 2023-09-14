5951dc8284b6d05d77bd3c4dd3ccccad82b6dd21
Ryan Huang <ryanhuan@andrew.cmu.edu>
2023-09-14 09:38:18 -0400

    one

--- __tests__/utils/QueryProcessor.test.ts
+++ __tests__/utils/QueryProcessor.test.ts
@@ -3,18 +3,11 @@ import '@testing-library/jest-dom'
 
 describe("QueryProcessor", () => {
     test("should return a string", () => {
-        const query = "test";
+        const query = "ryanhuan";
         const response: string = QueryProcessor(query);
         expect(typeof response).toBe("string");
+        expect(response).toBe("AndrewID: ryanhuan")
     });
 
-    test('should return shakespeare description', () => {
-        const query = "shakespeare";
-        const response: string = QueryProcessor(query);
-        expect(response).toBe((
-            "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
-            "English poet, playwright, and actor, widely regarded as the greatest " +
-            "writer in the English language and the world's pre-eminent dramatist."
-          ));
-    });
+   
 });
--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -1,9 +1,7 @@
 export default function QueryProcessor(query: string): string {
-  if (query.toLowerCase().includes("shakespeare")) {
+  if (query.toLowerCase().includes("ryanhuan")) {
     return (
-      "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
-      "English poet, playwright, and actor, widely regarded as the greatest " +
-      "writer in the English language and the world's pre-eminent dramatist."
+      "AndrewID: ryanhuan"
     );
   }
 
